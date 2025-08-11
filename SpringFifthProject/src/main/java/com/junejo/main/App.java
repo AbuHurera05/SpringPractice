e7bc779e382a25a331eff6c0e09ed29f20bc4e7c
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-08-11 15:31:17 +0500

    Update

--- SpringFifthProject/src/main/java/com/junejo/main/App.java
+++ SpringFifthProject/src/main/java/com/junejo/main/App.java
@@ -1,5 +1,10 @@
 package com.junejo.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.junejo.beans.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context=new ClassPathXmlApplicationContext("/com/junejo/resources/applicationContext.xml");
+    	
+    	Student std = (Student)  context.getBean("stdId");
+    	
+    	std.disply();
+    	
     }
 }
